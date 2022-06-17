1fe187348611e633b575e594ba985302bd62563e
Darryl Daniel <darrylndaniel@gmail.com>
2022-06-17 14:04:19 +0200

    :recycle: use a try catch around withdrawal functionality

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,20 @@ async function ask(question: string): Promise<string> {
     return userInput.input;
 }
 
-function canDispense(amount: number) {
-    // Todo: Implement this
-    // Hot tip: put this functionality in the main class
-    return Math.random() > 0.5;
-}
-
 async function run() {
     const result = await ask("How much would you like to withdraw?");
     let withdrawalAmount: number;
     try {
         withdrawalAmount = parseInt(result);
     } catch (e) {
-        console.log("Error: amount not a valid number: ", result);
+        console.error("Error: amount not a valid number: ", result);
         process.exit(1);
     }
-    if (canDispense(withdrawalAmount)) {
+    try {
         // Todo: Implement withdrawal functionality
         console.log("Withdrawing amount...");
-    } else {
-        console.log("Error: cannot dispense this amount - notes not available");
+    } catch (e) {
+        console.error("Error: cannot dispense this amount - notes not available");
         process.exit(1);
     }
     const balance = 0; // Todo: implement a function to get the balance left - bonus points for listing available notes
